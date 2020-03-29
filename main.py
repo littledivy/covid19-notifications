83634ba8dd27b3e4f93eeb93b019d6eb4e97716f
Divy Srivastava <dj.srivastava23@gmail,com>
2020-03-29 21:27:51 +0530

    :rocket: add better console output

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ from bs4 import BeautifulSoup
 from plyer import notification
 import time
 
+prev_cases = "0"
 def extract():
+  global prev_cases
   res = requests.get('https://www.worldometers.info/coronavirus/country/india').text
   soup = BeautifulSoup(res, 'html.parser')
   soup.encode('utf-8')
   cases = soup.find("div", { "class": "maincounter-number" }).get_text().strip()
-  notifyMe('Total Number of cases', cases)
+  if(cases == prev_cases):
+      print('['+ time.strftime('%l:%M:%S %p on %b %d, %Y') + ' ] NO CHANGE IN CASES')
+  else:
+    notifyMe('Total Number of cases', cases)
+    if(prev_cases != "0"):
+      print('[' + time.strftime('%l:%M:%S %p on %b %d, %Y') + ' ] CASES ROSE FROM ' + prev_cases + ' TO ' + cases)
+    prev_cases = cases
 
 def notifyMe(title, message):
   notification.notify(title=title, message=message, timeout=6)
